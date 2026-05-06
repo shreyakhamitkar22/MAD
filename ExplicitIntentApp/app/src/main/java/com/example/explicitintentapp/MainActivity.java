ae5646e233963d6dd9fbe29c66f94857e8a16b38
shreyakhamitkar22 <khamitkarshreya@gmail.com>
2026-05-06 14:15:21 +0530

    Add files via upload

---
+++ ExplicitIntentApp/app/src/main/java/com/example/explicitintentapp/MainActivity.java
@@ -0,0 +1,29 @@
+import com.example.explicitintentapp.R;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ ExplicitIntentApp/app/src/main/java/com/example/explicitintentapp/SecondActivity.java
@@ -0,0 +1,32 @@
+package com.example.explicitintentapp;
+
+import com.example.explicitintentapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
